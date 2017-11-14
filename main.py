c422cb768a10782a39621581868a46438eadda82
Your Name <you@example.com>
2017-11-14 18:00:57 -0300

    Corrigindo bug

--- main.py
+++ main.py
@@ -96,11 +96,11 @@ pass
 
 #Just to send informations each hour
 time_ticker = timeit.default_timer()
-telegram_send.send(['Firsbot Ping: We are starting at ' + str(datetime.now().ctime())
+telegram_send.send(['Firsbot Ping: We are starting at ' + str(datetime.now().ctime())])
 
 while 1:
     start = timeit.default_timer()
-    delta_time = start - time_ticker
+    # delta_time = start - time_ticker
     #Looking entry opportunities
     for c in combinations:
 
@@ -209,7 +209,7 @@ while 1:
 
     if delta_time >= 3600:
         time_ticker = timeit.default_timer()
-        telegram_send.send(['Firsbot Ping: We are alive at ' + str(datetime.now().ctime())
+        telegram_send.send(['Firsbot Ping: We are alive at ' + str(datetime.now().ctime())])
 
     stop =  timeit.default_timer()
     time.sleep(math.fabs(simulationTime - round(stop - start)))
